8edc996b46e2c5ff2256dd01a1b9a9e581fc8805
Rishi-Raj <rishirajpothanaboyina@gmail.com>
2026-09-13 22:13:34 +0530

    Add incident creation for monitor outages

--- server/workers/monitorWorker.js
+++ server/workers/monitorWorker.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import connectDB from "../config/database.js";
 import {Monitor} from "../models/Monitor.js";
 import {Check} from "../models/Check.js";
+import {Incident} from "../models/Incident.js";
 
 dotenv.config({
     path:'./.env'
@@ -19,9 +20,9 @@ const startMonitorWorker = async()=>{
             let monitorCount = monitors.length;
             console.log(`Due monitors: ${monitorCount}`);
             for(const monitor of monitors){
-                const url = monitor.url;
+                // const url = monitor.url;
                 // const url = "https://example.com/"; 
-                // const url = "https://this-domain-does-not-exist-12345.com";
+                const url = "https://this-domain-does-not-exist-12345.com";
                 // const url = "https://httpbin.org/delay/10"; // Simulating a slow response for testing
                 const requestSentAt = new Date();
                 let responseReceivedAt;
@@ -75,6 +76,8 @@ const startMonitorWorker = async()=>{
                     await check.save();
                     console.log("Check saved successfully");    
 
+                    let currentTime = new Date();
+
                     if(status === "UP"){
                         monitor.consecutiveSuccesses += 1;
                         monitor.consecutiveFailures = 0;
@@ -91,7 +94,7 @@ const startMonitorWorker = async()=>{
                         monitor.status = "UP";
                     }
 
-                    let currentTime = new Date();
+                    
                     if(currentTime - monitor.nextCheckAt <= monitor.interval * 1000){
                         monitor.nextCheckAt = new Date(monitor.nextCheckAt.getTime() + monitor.interval * 1000);
                     }
@@ -100,6 +103,24 @@ const startMonitorWorker = async()=>{
                     }
 
                     await monitor.save();
+                    let incidentStartedAt;
+                    if (monitor.consecutiveFailures === 2){
+                        const previousDownCheck = await Check.find({
+                            monitorId: monitor._id,
+                            status: "DOWN"
+                        }).sort({ requestSentAt: -1 }).skip(1).limit(1);
+                        incidentStartedAt = previousDownCheck[0].requestSentAt;
+                        console.log("Incident started at:", incidentStartedAt);
+
+                        const incident = new Incident({
+                            monitorId: monitor._id,
+                            incidentStartedAt: incidentStartedAt,
+                            status: "OPEN"
+                        })
+
+                        await incident.save();
+                        console.log("Incident created successfully");
+                    }
 
                 } catch(error){
                     console.log("Error while saving check:", error);
